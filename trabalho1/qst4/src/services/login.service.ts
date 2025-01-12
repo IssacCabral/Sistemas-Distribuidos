ad9cd076cc99e10dbfde162bebad71564b7c9f7a
Issac Cabral <clidenorissac@gmail.com>
2025-01-12 14:52:11 -0300

    refact: switch password

--- trabalho1/qst4/src/services/login.service.ts
+++ trabalho1/qst4/src/services/login.service.ts
@@ -7,15 +7,23 @@ export class LoginService extends BaseService {
   async askLogin() {
     const password = await question("Digite a senha:");
 
-    if (password === adminPassword || password === clientPassword) {
-      const request: Request = {
-        type: RequestType.LOGIN,
-        content: "Desejo Logar",
-      };
-      this.client.write(JSON.stringify(request));
-    } else {
-      console.log("Senha incorreta! Tente novamente.");
-      this.askLogin();
+    const request: Request = {
+      type: RequestType.LOGIN,
+      content: "",
+    };
+
+    switch (password) {
+      case adminPassword:
+        request.content = "admin";
+        this.client.write(JSON.stringify(request));
+        break;
+      case clientPassword:
+        request.content = "client";
+        this.client.write(JSON.stringify(request));
+        break;
+      default:
+        console.log("Senha incorreta! Tente novamente.");
+        this.askLogin();
     }
   }
 }
